a9eb50d26b090be93b75ab0282b645a8ccb65d78
Aleks Sobieraj <contact@aleks.tech>
2019-02-26 22:12:26 -0500

    Remove unused code

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -7,63 +7,6 @@ const { hasPermission } = require('../utils');
 const stripe = require('../stripe');
 
 const Mutations = {
-  async createItem(parent, args, ctx, info) {
-    if (!ctx.request.userId) {
-      throw new Error('You must be logged in to do that!');
-    }
-
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          // This is how to create a relationship between the Item and the User
-          user: {
-            connect: {
-              id: ctx.request.userId,
-            },
-          },
-          ...args,
-        },
-      },
-      info
-    );
-
-    console.log(item);
-
-    return item;
-  },
-  updateItem(parent, args, ctx, info) {
-    // first take a copy of the updates
-    const updates = { ...args };
-    // remove the ID from the updates
-    delete updates.id;
-    // run the update method
-    return ctx.db.mutation.updateItem(
-      {
-        data: updates,
-        where: {
-          id: args.id,
-        },
-      },
-      info
-    );
-  },
-  async deleteItem(parent, args, ctx, info) {
-    const where = { id: args.id };
-    // 1. find the item
-    const item = await ctx.db.query.item({ where }, `{ id title user { id }}`);
-    // 2. Check if they own that item, or have the permissions
-    const ownsItem = item.user.id === ctx.request.userId;
-    const hasPermissions = ctx.request.user.permissions.some(permission =>
-      ['ADMIN', 'ITEMDELETE'].includes(permission)
-    );
-
-    if (!ownsItem && !hasPermissions) {
-      throw new Error("You don't have permission to do that!");
-    }
-
-    // 3. Delete it!
-    return ctx.db.mutation.deleteItem({ where }, info);
-  },
   async createEntry(parent, args, ctx, info) {
     if (!ctx.request.userId) {
       throw new Error('You must be logged in to do that!');
